a6af45aa5e228400b4da4bf310a1ef99688f5c92
Maclemon17 <maclemon17@gmail.com>
2021-12-01 14:00:53 +0100

    Nav Bar Fixed

--- resources/views/partials/_navBar.blade.php
+++ resources/views/partials/_navBar.blade.php
@@ -10,45 +10,38 @@
                     <ul>
                         <li class="active"><a href="{{ route('home') }}">Home</a></li>
                         <li><a href="{{ route('mou') }}">M0U</a></li>
-                        {{-- <li class="drop-down"><a href="">Account</a> --}}
-                            {{-- <li> --}}
-                                <!-- Authentication Links -->
-                                @guest
-                                    <li class="nav-item">
+                        @guest
+                            <li class="drop-down"><a href="#"><i class="icofont-ui-user icofont-lg"></i></a>
+                                <ul>
+                                    <li class="">
                                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                     </li>
-                                    {{-- @if (Route::has('register'))
-                                        <li class="nav-item">
-                                            <a class="nav-link"
-                                                href="{{ route('register') }}">{{ __('Register') }}</a>
-                                        </li>
-                                    @endif --}}
-                                @else
-                                    <li class="nav-item dropdown">
-                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            {{ Auth::user()->name }} <span class="caret"></span>
-                                        </a>
+                                </ul>
+                            </li>
+                        @else
+                            <li class="drop-down dropdown">
+                                <a id="navbarDropdown" class="" href="#" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false" v-pre> <i class="icofont-ui-user icofont-lg"></i>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
-                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                                {{ __('Logout') }}
-                                            </a>
+                                <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                 document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                                style="display: none;">
-                                                @csrf
-                                            </form>
-                                        </div>
-                                    </li>
-                                @endguest
-                            {{-- </li> --}}
-                        {{-- </li> --}}
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                     </ul>
                 </nav><!-- .nav-menu -->
                 @yield('navBtn')
-               
+
             </div>
         </div>
     </div>
