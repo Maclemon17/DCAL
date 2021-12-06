1be541eac9c96caf7a4d89e61ddf603be5d4eaac
Maclemon17 <maclemon17@gmail.com>
2021-12-06 13:01:20 +0100

    storage

--- app/Http/Controllers/MoUController.php
+++ app/Http/Controllers/MoUController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Stroage;
+use Illuminate\Support\Facades\Storage;
 use App\Post;
 
 class MoUController extends Controller
